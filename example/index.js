b638bf8f076acc20926581d66455d2db926eec41
Ionică Bizău <bizauionica@gmail.com>
2016-06-06 10:23:58 +0300

    Examples

--- example/index.js
+++ example/index.js
@@ -2,4 +2,29 @@
 
 const mdify = require("../lib");
 
-console.log(mdify());
+let md = mdify.stringify({
+    author: "Johnny B."
+  , date: new Date()
+  , tags: ["hello", "world"]
+}, `## Hello World!
+Hey **there**!`);
+// ---
+// author: Johnny B.
+// date: 2016-06-06T07:12:17.533Z
+// tags:
+//   - hello
+//   - world
+// ---
+// ## Hello World!
+// Hey **there**!
+
+console.log(md);
+
+console.log(mdify.parse(md));
+// { markdown: '\n## Hello World!\nHey **there**!',
+//   metadata:
+//    { author: 'Johnny B.',
+//      date: Mon Jun 06 2016 10:12:17 GMT+0300 (EEST),
+//      tags: [ 'hello', 'world' ] },
+//   rawMeta: '---\nauthor: Johnny B.\ndate: 2016-06-06T07:12:17.533Z\ntags:\n  - hello\n  - world',
+//   html: '<h2 id="helloworld">Hello World!</h2>\n\n<p>Hey <strong>there</strong>!</p>' }
